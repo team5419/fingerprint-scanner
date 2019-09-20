00596418204beca5b9ddb4312808314c899f86ce
FelixMo42 <felix@tacocat.com>
2019-09-20 09:17:58 -0700

    looking good

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
 import requests
+import getpass
 
-res = requests.post(
-    "https://timesheet.team5419.org/sessions",
-    data={
-        "email" : "",
-        "password" : ""
-    }
-)
-
-session_key = res.cookies.get("_timesheet_session")
-
-def loguser(key):
-    res = requests.post(
-        "http://timesheet.team5419.org/timelogs",
+def login(session, email, password):
+    res = session.post(
+        "https://timesheet.team5419.org/sessions",
         data={
-            "authenticity_token": session_key,
+            "email" : email,
+            "password" : password
+        }
+    )
+
+    print(res.status_code)
+
+    print("logged in!")
+
+    return res.cookies.get("_timesheet_session")
+
+def logout(session):
+    res = session.get(
+        "https://timesheet.team5419.org/log_out"
+    )
+
+    print(res.status_code)
+
+    print("logged out!")
+
+def loguser(session, key):
+    print(session.cookies["_timesheet_session"])
+    res = session.post(
+        "https://timesheet.team5419.org/timelogs",
+        data={
+            "authenticity_token": session.cookies,
             "owner_userid": key,
             "multi": "Submit"
         }
     )
 
-  return res
+    print(res.text)
 
-loguser(1)
+    return res
+
+with requests.Session() as session:
+    login(
+        session=session,
+        email="felix@tacocat.com",#input("Enter email: "),
+        password="felixmo2006894"#getpass.getpass()
+    )
+
+    loguser(session, 2)
+
+    logout(session) #logout dosent work
